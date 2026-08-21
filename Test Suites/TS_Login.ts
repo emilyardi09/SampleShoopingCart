2567a92afc939c40bfb9f61199a6c84fa035cbad
endahwahyuni <endahwahyuni@localhost>
2026-08-21 07:16:33 +0700

    commit 3

---
+++ Test Suites/TS_Login.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <maxConcurrentInstances>1</maxConcurrentInstances>
+   <numberOfRerun>0</numberOfRerun>
+   <orchestration>CLASSIC</orchestration>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9594cc12-1f4b-46a0-a5cf-fcc62fec2907</testSuiteGuid>
+</TestSuiteEntity>
